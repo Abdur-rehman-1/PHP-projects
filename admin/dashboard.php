aea16256244bd1fad276093901c301ca889e0f4e
Abdur-rehman-1 <rehmanabd129@gmail.com>
2026-01-29 19:07:44 +0500

    Initial commit: SCHOOL-MANAGMENT-SYSTEM

---
+++ admin/dashboard.php
@@ -0,0 +1,29 @@
+<?php
+
+  include "../includes/config.php";
+  include "../includes/header.php";
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+<body>
+    <nav class="nav  bg-dark p-3 ">
+        <li class="nav-item active">
+            <a class="nav-link active" href="#">Item 1</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Item 2</a>
+        </li>
+    </nav>
---
+++ includes/config.php
@@ -0,0 +1,7 @@
+<?php
+
+$connection = mysqli_connect("localhost", "root", "", "school-managment-system") or die("Connection failed: " . mysqli_connect_error());
+  
+
+
+?>
